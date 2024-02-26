366f8c4fb35090bed22d8a17c7114bc70e089894
Sarah Chung <126906153+Sarah-Chung@users.noreply.github.com>
2024-02-26 14:33:09 -0500

    Update num_to_matrix.py

--- num_to_matrix.py
+++ num_to_matrix.py
@@ -3,6 +3,7 @@ import csv
 inputFile = 'PF16725_msa_numerical.txt'
 outputFile = 'PF16725_msa_numerical_matrix.csv'
 
+# Converting msa_numerical.txt output from bmDCA to .csv
 def numToMatrix(input, output):
     with open(input, 'r') as file, open(output, 'w', newline = '') as outputFile:
         csv_writer = csv.writer(outputFile)
@@ -12,6 +13,9 @@ def numToMatrix(input, output):
         next(file)
         for line in file:
             eachLine = line.split()
+            for i in range(len(eachLine)):
+                eachLine[i] = int(eachLine[i])
+                eachLine[i]+=1
             print(eachLine)
             csv_writer.writerow(eachLine)
     outputFile.close()
